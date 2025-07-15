49d90ed5dc54c2f1bb85e88b026e12273bb6685a
Muhammad Zubair Saleem <mzubairsaleem@outlook.com>
2025-07-15 16:57:59 +0000

    SetUP

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -20,13 +20,13 @@ module.exports = {
   deploy: {
     production: {
       user: 'root',
-      host: ['91.99.74.62', '116.203.216.30', '116.203.150.214', '159.69.93.168', '91.99.140.74', '116.203.107.5'],
+      host: ['178.156.128.242', '178.156.165.54', '5.161.242.149', '5.161.114.27'],
       ref: 'origin/main',
       repo: 'https://github.com/mzubairsaleem/ws-server.git',
       path: '/opt/wss-server',
       'pre-deploy': 'source ~/.nvm/nvm.sh',
       'post-deploy': 'source ~/.nvm/nvm.sh  && npm install && pm2 startOrRestart ecosystem.config.cjs --env production',
-      'pre-setup': 'source ~/.nvm/nvm.sh && nvm use 16 && npm install -g pm2@latest && pm2 startup'
+      'pre-setup': 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash && source ~/.nvm/nvm.sh && nvm install 16 && nvm use 16 && npm install -g pm2@latest && pm2 startup'
     }
   }
 };
